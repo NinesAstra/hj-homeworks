ca22d526218ae8327dfb95726b2c2df7824538d8
NinesAstra <nines1984@mail.ru>
2019-03-27 22:49:55 +0400

    create drum-machine

---
+++ html-element-collection/drum-machine/js/js.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const liCollection = document.getElementsByClassName('drum-kit__drum');
+let prev = 0;
+console.log(prev);
+function music() {
+	if(prev !== 0){prev.pause(); prev.currentTime = 0;}
+	const next = this.getElementsByTagName('audio')[0];
+	next.play();
+	prev = next;
+	console.log(prev);
+	return prev;
+};
+
+for (const li of liCollection) {
+	li.onclick = music;
+};
