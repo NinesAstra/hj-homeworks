5459588a6b0ce41b1e767dbc1482d0a7f82dac05
NinesAstra <nines1984@mail.ru>
2019-04-03 00:04:56 +0400

    сделала список контактов

---
+++ dom/contact-list/js/li-js.js
@@ -0,0 +1,12 @@
+const listStr = loadContacts();
+const lists = JSON.parse(listStr);
+const ul = document.querySelector('.contacts-list');
+
+const arrLi = [];
+let li = 0;
+for (const list of lists) {
+	li = '<li data-email="'+list.email+'" data-phone="'+list.phone+'"><strong>'+list.name+'</strong></li>';
+	arrLi.push(li);
+}
+
+ul.innerHTML = arrLi.join('');
