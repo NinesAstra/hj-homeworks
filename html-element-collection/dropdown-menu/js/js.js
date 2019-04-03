3c79ebaa3f76c6225294730ed0d0400b5631f8ab
NinesAstra <nines1984@mail.ru>
2019-04-03 13:48:47 +0400

    упростила решение 1го задания

--- html-element-collection/dropdown-menu/js/js.js
+++ html-element-collection/dropdown-menu/js/js.js
@@ -1,16 +1,18 @@
 'use strict'
-const btnMenu = document.getElementsByClassName('wrapper-dropdown')[0],
-dropdown = document.getElementsByClassName('dropdown')[0],
-lis = dropdown.getElementsByTagName('li');
+const btnMenu = document.getElementsByClassName('wrapper-dropdown')[0];
+//dropdown = document.getElementsByClassName('dropdown')[0],
+//lis = dropdown.getElementsByTagName('li');
 
 btnMenu.onclick = toggle;
- for (const li of lis) {
- 	li.onclick = removeClass;
- }
-
 function toggle () {
 	console.log(this);
 	btnMenu.classList.toggle('active');}
-function removeClass () {
-	console.log(lis);
-	btnMenu.classList.remove();};
+
+// btnMenu.onclick = toggle;
+//  for (const li of lis) {
+//  	li.onclick = removeClass;
+//  }
+
+// function removeClass () {
+// 	console.log(lis);
+// 	btnMenu.classList.remove();};
