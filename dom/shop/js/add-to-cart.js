f1bd2d2948a01e1d77c826de57254b2cdf60902f
NinesAstra <nines1984@mail.ru>
2019-04-09 09:34:00 +0400

    переименовала функцию добавления в корзину

--- dom/shop/js/add-to-cart.js
+++ dom/shop/js/add-to-cart.js
@@ -4,11 +4,11 @@ totalPrice = document.querySelector('span#cart-total-price');
 
 
 for(let btnAdd of btnsAdd){
-	btnAdd.addEventListener('click', add);
+	btnAdd.addEventListener('click', addInformationToCart);
 }
 
 
-function add(event) {
+function addInformationToCart(event) {
 	count.innerHTML = Number(count.innerHTML)+1;
 	let valueTotalPrice = Number(totalPrice.innerHTML.replace(/\s/g, "")) + Number(event.currentTarget.dataset.price);
 	totalPrice.innerHTML = getPriceFormatted(valueTotalPrice);
