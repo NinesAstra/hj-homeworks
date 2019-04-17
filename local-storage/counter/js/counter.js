0fa9c997f2680a0df75c9ea2351d2eff05e8d032
NinesAstra <nines1984@mail.ru>
2019-04-17 22:40:04 +0400

    сделала меню

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,6 @@
 'use strict';
+const counter = document.querySelector('#counter');
+const increment = document.querySelector('#increment');
+const decrement = document.querySelector('#decrement');
+const reset = document.querySelector('reset');
+
