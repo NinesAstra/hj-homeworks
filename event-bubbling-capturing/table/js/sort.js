4027294158b85501a1412b8551ba40f24d93cafb
NinesAstra <nines1984@mail.ru>
2019-04-17 22:49:37 +0400

    сделала table

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+   if (event.target.classList.contains('prop__name')) {
+  if (count % 2 === 0) { 
+  	event.target.dataset.dir = 1;
+  } else {
+  	event.target.dataset.dir = -1;
+  }
+count++;
+sortTable(event.target.dataset.propName, event.target.dataset.dir);
+  }
 }
