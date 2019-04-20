f8f86437a3ca4c9c13a85b701481203592e646dd
NinesAstra <nines1984@mail.ru>
2019-04-20 14:00:19 +0400

    убрала счетчик из логики

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,7 +1,18 @@
 'use strict';
 
-let count = 0;
+function handleTableClick(event) {
 
+   if (!event.target.classList.contains('prop__name')) {
+	   return false;
+	}
+	console.log(event.target)
+    event.target.dataset.dir == 1 ?
+  	event.target.dataset.dir = -1 :
+  	event.target.dataset.dir = 1;
+sortTable(event.target.dataset.propName, event.target.dataset.dir);
+}
+
+/* 1 вариант
 function handleTableClick(event) {
    if (event.target.classList.contains('prop__name')) {
   if (count % 2 === 0) { 
@@ -12,4 +23,4 @@ function handleTableClick(event) {
 count++;
 sortTable(event.target.dataset.propName, event.target.dataset.dir);
   }
-}
+}*/
