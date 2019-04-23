17dd97d67aef5372bd66906bbfcabcab63edd812
NinesAstra <nines1984@mail.ru>
2019-04-24 01:28:46 +0400

    сделала задание 1

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,12 @@
 'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+connection.addEventListener('open', showBubbles(connection));
+document.addEventListener('click', event => {
+	const click = {
+	x: event.pageX,
+	y: event.pageY
+	}
+	console.log(click)
+	connection.send(JSON.stringify(click));
+})
+
