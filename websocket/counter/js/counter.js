af4f7f711dd62c0f9d6dc4a0eb10e99589132717
NinesAstra <nines1984@mail.ru>
2019-04-24 13:22:26 +0400

    сделала задание 2

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,26 @@
 'use strict';
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('.errors');
+
+function innerInfo(info) {
+	console.log(info);
+	counter.textContent = info.connections;
+	errors.value = info.errors;
+}
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+connection.addEventListener('open', event => {
+console.log(event);
+});
+connection.addEventListener('message', event => {
+const info = JSON.parse(event.data);
+innerInfo(info);
+});
+connection.addEventListener('error', error => {
+console.log(`Произошла ошибка: ${error.data}`);
+});
+window.addEventListener('beforeunload', () => {
+connection.close(1000, 'Работа закончена');
+});
+
+
