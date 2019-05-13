b6fbaa467b8f69325aa6200b76ad60e3a779d268
NinesAstra <nines1984@mail.ru>
2019-05-13 23:39:44 +0400

    last commit media

--- media/eye/js/animation.js
+++ media/eye/js/animation.js
@@ -1,1 +1,53 @@
-'use strict';
+const bigBookPupil = document.querySelector('.big-book__pupil'),
+      bigBookEye = document.querySelector('.big-book__eye'),
+      body = document.querySelector('body');
+// текущая позиция центра глаза на странице
+let currentPositionEye = {x: 0, y: 0};
+
+// перемещение зрачка
+function moveEye(event) {
+  // координаты курсора
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  /* определяем базовое расстояние, при удалении на которое 
+  размер зрачка будет равен 1, а смещение от центра глаза - 30px.
+  Можно рассчитать это расстояние и до каждой границы экрана, по заданию размер зрачка
+  должен быть минимальным, когда курсор мыши находится близко к краю окна браузера. 
+  А смещение выбирается пропорционально тому, насколько курсор мыши далеко от центра глаза. 
+  Но ведь может возникнуть ситуация, когда граница экрана, например нижняя очень близко
+  к центру глаза, в таком случае  придется изменять размеры зрачка и смещение от 
+  минимальных значений до максимальных на очень ограниченном расстоянии... Для получения
+  максимального сходства с движением глаза, чтобы на одинаковом расстоянии от центра
+  в любом направлении сохранялся одинаковый размер зрачка, а также отклонение и применяется 
+  это базовое   расстояние. Т.е. в   большинстве случаев ширина экрана больше высоты, то в 
+  качестве этого параметра выбрана  половина ширины экрана.
+  */
+  let baseDistance = body.clientWidth/2;
+  // определяем координаты body
+  const positionAndSizeBody = body.getBoundingClientRect();   
+  // определяем центр глазного яблока
+  const positionAndSizeBookEye = bigBookEye.getBoundingClientRect();
+  currentPositionEye.x = positionAndSizeBookEye.left + (positionAndSizeBookEye.width)/2;
+  currentPositionEye.y = positionAndSizeBookEye.top + (positionAndSizeBookEye.height)/2;
+  // смещение мыши относительно центра глаза
+  const differenceX = mouseX - currentPositionEye.x,
+        differenceY = mouseY - currentPositionEye.y,
+        // расстояние от курсора до центра глаза
+        distance = Math.sqrt(Math.pow(differenceX, 2) + Math.pow(differenceY, 2));  
+  // кооэффициент для вычисления смещения  
+  let ratioEyeSize = distance/baseDistance > 1 ? 1 : distance/baseDistance; 
+  // если расстояние от курсора до края глазного яблока больше базового смещения
+  // - принимаем равным 1, если курсор зашел на глазное яблоко - 0
+  let ratioEyeball = (distance - positionAndSizeBookEye.width/2)/baseDistance > 1 ? 1 : (distance - positionAndSizeBookEye.width/2)/baseDistance;
+  // вычисляем размер и смещения зрачка
+  const sizePupil = 1 + 2*(1 - ratioEyeball),
+        posXEye = 30*ratioEyeSize*differenceX/distance,
+        posYEye = 30*ratioEyeSize*differenceY/distance;
+  
+  // устанавливае вычесленные свойства
+  bigBookPupil.style.setProperty('--pupil-x', posXEye + 'px');
+  bigBookPupil.style.setProperty('--pupil-y', posYEye + 'px');
+  bigBookPupil.style.setProperty('--pupil-size', sizePupil);
+}
+// обработчик при перемещении мыши
+document.addEventListener('mousemove', moveEye);
--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,1 +1,116 @@
 'use strict';
+const app = document.querySelector('.app'),
+      controls = document.querySelector('.controls'),
+      list = document.querySelector('.list'),
+      buttonTakePhoto = document.getElementById('take-photo'),
+      errorMessage = document.getElementById('error-message');
+// создаем аудиотег и устанавливаем у него src, нужен будет для звука при нажатии на кнопку сделать фото
+const photoSound = document.createElement('audio');
+photoSound.src = 'https://raw.githubusercontent.com/netology-code/hj-homeworks/master/media/photo-booth/audio/click.mp3';
+// создаем видеотег и вставляем его перед controls
+const video = document.createElement('video');
+app.insertBefore(video, controls);
+// создаем холст и получаем его контекст
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+// получаем доступ к видеокамере
+navigator.mediaDevices
+  .getUserMedia({video: true, audio: false})
+  .then((stream) => { 
+    // если все гуд - воспроизводим видео в видеотеге
+    video.srcObject=stream;
+    video.play();
+    controls.style.display = 'flex';    
+  })
+  // если все плохо - выводим ошибку
+  .catch(err => {
+    console.warn(err);
+    errorMessage.classList.add('visible');
+    errorMessage.innerText = 'Нет доступа к видеокамере!';
+  });
+
+function takeFoto() {
+  // проверяем размеры холста
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  // копируем текущий кадр видеотега в canvas
+  ctx.drawImage(video, 0, 0);
+  // обновляем src
+  const src = canvas.toDataURL();  
+  // создаем карточку фото  
+  photoSound.pause();
+  photoSound.currentTime = 0;
+  photoSound.play(); 
+  createPhotoCard(src, 'snapshot.png');  
+}
+
+// функция создания элемента
+function el(tagName, attributes, children) {
+  const element = document.createElement(tagName);
+  if (typeof attributes === 'object') {
+    Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+  }
+  if (typeof children === 'string') {
+    element.innerText = children;
+  } else if (children instanceof Array) {
+    children.forEach(child => element.appendChild(child));
+  }
+  return element;
+}
+
+// создание карточки фотографии
+function createPhotoCard(src, download) {
+  const photoCard =  el('figure', {class: ''}, [
+            el('img', {src: src}, ''),
+            el('figcaption', {class: ''}, [
+              el('a', {href: src, download: download}, [
+                el('i', {class: 'material-icons file_download'}, 'file_download') 
+              ]),
+              el('a', {class: ''}, [
+                el('i', {class: 'material-icons file_upload'}, 'file_upload') 
+              ]),
+              el('a', {class: ''}, [
+                el('i', {class: 'material-icons delete'}, 'delete') 
+              ])
+            ])
+         ]);
+  // добавляем фото 
+  list.appendChild(photoCard);  
+  // навешиваем обработчики на кнопки
+  photoCardAddEventListener(photoCard, src);  
+}
+// обработчик на кнопку сделать фото
+buttonTakePhoto.addEventListener('click', takeFoto);
+
+// функция навешивания обработчиков на кнопки новой карточки фото
+function photoCardAddEventListener(photoCard, url) {
+  // удалить фото
+  const btnDelete = photoCard.querySelector('a i.delete'); 
+  btnDelete.addEventListener('click', () => {    
+    photoCard.remove();
+  });
+  // скачать фото
+  const btnDownload = photoCard.querySelector('a i.file_download');
+  btnDownload.addEventListener('click', () => {
+    btnDownload.remove();
+  });
+  // загрузить фото на сервер
+  const btnUpload = photoCard.querySelector('a i.file_upload');
+  
+  btnUpload.addEventListener('click', (event) => { 
+    event.preventDefault();
+    fetch(url)
+      .then(res => res.blob())
+      .then(blob => {
+        const dataForServer = new FormData();
+        dataForServer.append('image', blob); 
+        // Загружаем данные на сервер
+        fetch('https://neto-api.herokuapp.com/photo-booth', {method: 'POST', body: dataForServer})
+          .then(res => {
+            console.log(res);
+            btnUpload.remove();          
+          });
+      });      
+  });  
+}
